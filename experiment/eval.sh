9289146ca5b4c5b306cef749f75d856f284e360f
Mustafa974 <920998500@qq.com>
2022-11-29 15:52:38 +0000

    feat(visual): add visualizer

--- experiment/eval.sh
+++ experiment/eval.sh
@@ -2,21 +2,22 @@
 
 # this example uses a single node (`NUM_NODES=1`) w/ 4 GPUs (`NUM_GPUS_PER_NODE=4`)
 export NUM_NODES=1
-export NUM_GPUS_PER_NODE=4
+export NUM_GPUS_PER_NODE=1
 export NODE_RANK=0
 export WORLD_SIZE=$(($NUM_NODES * $NUM_GPUS_PER_NODE))
-export CUDA_VISIBLE_DEVICES="4,5,6,7"
+export CUDA_VISIBLE_DEVICES="6"
 export TORCH_DISTRIBUTED_DEBUG="DETAIL"
 # master address
 export MASTER_ADDR="localhost"
-export MASTER_PORT="12343"
+export MASTER_PORT="12344"
 # wandb group name
 export WANDB_GROUP=$(date "+%Y%m%dT%H%M%S")
 # set python path
 export PYTHONPATH=$PWD
 echo $PYTHONPATH
 # load checkpoint
-export CHECKPOINT_PATH="runs/train/exp11/weights/Epoch17.pt"
+# export CHECKPOINT_PATH="old-runs/train/exp75/weights/Epoch4.pt"
+export CHECKPOINT_PATH="runs/train/letong/weights/Epoch28.pt"
 # predict test dataset.
 export SAVE_PREDICT=1
 

03e8db55e0d755f8de6669e5917dc90fb0579c06
xmx-521 <xmx20010521@gmail.com>
2023-04-03 06:36:33 +0000

    feat(model): add Diffusion-CR model

---
+++ experiment/models/diffusion_cr.py
@@ -0,0 +1,410 @@
+import math
+import torch
+import numpy as np
+from torch import nn
+from tqdm import tqdm
+from functools import partial
+from inspect import isfunction
+
+class Diffusion_CR(nn.Module):
+    def __init__(self, beta_schedule):
+        super().__init__()
+        self.denoise_fn = UNet()
+        self.beta_schedule = beta_schedule
+
+    def set_loss(self, loss_fn):
+        self.loss_fn = loss_fn
+
+    def set_new_noise_schedule(self, device=torch.device('cuda'), phase='train'):
+        to_torch = partial(torch.tensor, dtype=torch.float32, device=device)
+        betas = make_beta_schedule(**self.beta_schedule[phase])
+        betas = betas.detach().cpu().numpy() if isinstance(
+            betas, torch.Tensor) else betas
+        alphas = 1. - betas
+
+        timesteps, = betas.shape
+        self.num_timesteps = int(timesteps)
+        
+        gammas = np.cumprod(alphas, axis=0)
+        gammas_prev = np.append(1., gammas[:-1])
+
+        # calculations for diffusion q(x_t | x_{t-1}) and others
+        self.register_buffer('gammas', to_torch(gammas))
+        self.register_buffer('sqrt_recip_gammas', to_torch(np.sqrt(1. / gammas)))
+        self.register_buffer('sqrt_recipm1_gammas', to_torch(np.sqrt(1. / gammas - 1)))
+
+        # calculations for posterior q(x_{t-1} | x_t, x_0)
+        posterior_variance = betas * (1. - gammas_prev) / (1. - gammas)
+        # below: log calculation clipped because the posterior variance is 0 at the beginning of the diffusion chain
+        self.register_buffer('posterior_log_variance_clipped', to_torch(np.log(np.maximum(posterior_variance, 1e-20))))
+        self.register_buffer('posterior_mean_coef1', to_torch(betas * np.sqrt(gammas_prev) / (1. - gammas)))
+        self.register_buffer('posterior_mean_coef2', to_torch((1. - gammas_prev) * np.sqrt(alphas) / (1. - gammas)))
+
+    def predict_start_from_noise(self, y_t, t, noise):
+        return (
+            extract(self.sqrt_recip_gammas, t, y_t.shape) * y_t -
+            extract(self.sqrt_recipm1_gammas, t, y_t.shape) * noise
+        )
+
+    def q_posterior(self, y_0_hat, y_t, t):
+        posterior_mean = (
+            extract(self.posterior_mean_coef1, t, y_t.shape) * y_0_hat +
+            extract(self.posterior_mean_coef2, t, y_t.shape) * y_t
+        )
+        posterior_log_variance_clipped = extract(self.posterior_log_variance_clipped, t, y_t.shape)
+        return posterior_mean, posterior_log_variance_clipped
+
+    def p_mean_variance(self, y_t, t, clip_denoised: bool, y_cond=None):
+        noise_level = extract(self.gammas, t, x_shape=(1, 1)).to(y_t.device)
+        y_0_hat = self.predict_start_from_noise(
+                y_t, t=t, noise=self.denoise_fn(torch.cat([y_cond, y_t], dim=1), noise_level))
+
+        if clip_denoised:
+            y_0_hat.clamp_(-1., 1.)
+
+        model_mean, posterior_log_variance = self.q_posterior(
+            y_0_hat=y_0_hat, y_t=y_t, t=t)
+        return model_mean, posterior_log_variance
+
+    def q_sample(self, y_0, sample_gammas, noise=None):
+        noise = default(noise, lambda: torch.randn_like(y_0))
+        return (
+            sample_gammas.sqrt() * y_0 +
+            (1 - sample_gammas).sqrt() * noise
+        )
+
+    @torch.no_grad()
+    def p_sample(self, y_t, t, clip_denoised=True, y_cond=None):
+        model_mean, model_log_variance = self.p_mean_variance(
+            y_t=y_t, t=t, clip_denoised=clip_denoised, y_cond=y_cond)
+        noise = torch.randn_like(y_t) if any(t>0) else torch.zeros_like(y_t)
+        return model_mean + noise * (0.5 * model_log_variance).exp()
+
+    @torch.no_grad()
+    def restoration(self, y_cond, y_t=None, y_0=None, mask=None, sample_num=8):
+        b, *_ = y_cond.shape
+
+        assert self.num_timesteps > sample_num, 'num_timesteps must greater than sample_num'
+        sample_inter = (self.num_timesteps//sample_num)
+        
+        y_t = default(y_t, lambda: torch.randn_like(y_cond))
+        ret_arr = y_t
+        for i in tqdm(reversed(range(0, self.num_timesteps)), desc='sampling loop time step', total=self.num_timesteps):
+            t = torch.full((b,), i, device=y_cond.device, dtype=torch.long)
+            y_t = self.p_sample(y_t, t, y_cond=y_cond)
+            if mask is not None:
+                y_t = y_0*(1.-mask) + mask*y_t
+            if i % sample_inter == 0:
+                ret_arr = torch.cat([ret_arr, y_t], dim=0)
+        return y_t, ret_arr
+
+    def forward(self, y_0, y_cond=None, mask=None, noise=None):
+        # sampling from p(gammas)
+        b, *_ = y_0.shape
+        t = torch.randint(1, self.num_timesteps, (b,), device=y_0.device).long()
+        gamma_t1 = extract(self.gammas, t-1, x_shape=(1, 1))
+        sqrt_gamma_t2 = extract(self.gammas, t, x_shape=(1, 1))
+        sample_gammas = (sqrt_gamma_t2-gamma_t1) * torch.rand((b, 1), device=y_0.device) + gamma_t1
+        sample_gammas = sample_gammas.view(b, -1)
+
+        noise = default(noise, lambda: torch.randn_like(y_0))
+        y_noisy = self.q_sample(
+            y_0=y_0, sample_gammas=sample_gammas.view(-1, 1, 1, 1), noise=noise)
+
+        if mask is not None:
+            noise_hat = self.denoise_fn(torch.cat([y_cond, y_noisy*mask+(1.-mask)*y_0], dim=1), sample_gammas)
+            loss = self.loss_fn(mask*noise, mask*noise_hat)
+        else:
+            noise_hat = self.denoise_fn(torch.cat([y_cond, y_noisy], dim=1), sample_gammas)
+            loss = self.loss_fn(noise, noise_hat)
+        return loss
+
+class UNet(nn.Module):
+    def __init__(
+        self,
+        in_channel=28,
+        out_channel=13,
+        inner_channel=32,
+        norm_groups=32,
+        channel_mults=(1, 2, 4, 8, 8),
+        attn_res=(8),
+        res_blocks=3,
+        dropout=0,
+        with_noise_level_emb=True,
+        image_size=128
+    ):
+        super().__init__()
+
+        if with_noise_level_emb:
+            noise_level_channel = inner_channel
+            self.noise_level_mlp = nn.Sequential(
+                PositionalEncoding(inner_channel),
+                nn.Linear(inner_channel, inner_channel * 4),
+                Swish(),
+                nn.Linear(inner_channel * 4, inner_channel)
+            )
+        else:
+            noise_level_channel = None
+            self.noise_level_mlp = None
+
+        num_mults = len(channel_mults)
+        pre_channel = inner_channel
+        feat_channels = [pre_channel]
+        now_res = image_size
+        downs = [nn.Conv2d(in_channel, inner_channel,
+                           kernel_size=3, padding=1)]
+        for ind in range(num_mults):
+            is_last = (ind == num_mults - 1)
+            use_attn = (now_res in attn_res)
+            channel_mult = inner_channel * channel_mults[ind]
+            for _ in range(0, res_blocks):
+                downs.append(ResnetBlocWithAttn(
+                    pre_channel, channel_mult, noise_level_emb_dim=noise_level_channel, norm_groups=norm_groups, dropout=dropout, with_attn=use_attn))
+                feat_channels.append(channel_mult)
+                pre_channel = channel_mult
+            if not is_last:
+                downs.append(Downsample(pre_channel))
+                feat_channels.append(pre_channel)
+                now_res = now_res//2
+        self.downs = nn.ModuleList(downs)
+
+        self.mid = nn.ModuleList([
+            ResnetBlocWithAttn(pre_channel, pre_channel, noise_level_emb_dim=noise_level_channel, norm_groups=norm_groups,
+                               dropout=dropout, with_attn=True),
+            ResnetBlocWithAttn(pre_channel, pre_channel, noise_level_emb_dim=noise_level_channel, norm_groups=norm_groups,
+                               dropout=dropout, with_attn=False)
+        ])
+
+        ups = []
+        for ind in reversed(range(num_mults)):
+            is_last = (ind < 1)
+            use_attn = (now_res in attn_res)
+            channel_mult = inner_channel * channel_mults[ind]
+            for _ in range(0, res_blocks+1):
+                ups.append(ResnetBlocWithAttn(
+                    pre_channel+feat_channels.pop(), channel_mult, noise_level_emb_dim=noise_level_channel, norm_groups=norm_groups,
+                        dropout=dropout, with_attn=use_attn))
+                pre_channel = channel_mult
+            if not is_last:
+                ups.append(Upsample(pre_channel))
+                now_res = now_res*2
+
+        self.ups = nn.ModuleList(ups)
+
+        self.final_conv = Block(pre_channel, default(out_channel, in_channel), groups=norm_groups)
+
+    def forward(self, x, time):
+        t = self.noise_level_mlp(time) if exists(
+            self.noise_level_mlp) else None
+
+        feats = []
+        for layer in self.downs:
+            if isinstance(layer, ResnetBlocWithAttn):
+                x = layer(x, t)
+            else:
+                x = layer(x)
+            feats.append(x)
+
+        for layer in self.mid:
+            if isinstance(layer, ResnetBlocWithAttn):
+                x = layer(x, t)
+            else:
+                x = layer(x)
+
+        for layer in self.ups:
+            if isinstance(layer, ResnetBlocWithAttn):
+                x = layer(torch.cat((x, feats.pop()), dim=1), t)
+            else:
+                x = layer(x)
+
+        return self.final_conv(x)
+
+
+# PositionalEncoding Source： https://github.com/lmnt-com/wavegrad/blob/master/src/wavegrad/model.py
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, noise_level):
+        count = self.dim // 2
+        step = torch.arange(count, dtype=noise_level.dtype, device=noise_level.device) / count
+        encoding = noise_level.unsqueeze(1) * torch.exp(-math.log(1e4) * step.unsqueeze(0))
+        encoding = torch.cat([torch.sin(encoding), torch.cos(encoding)], dim=-1)
+        return encoding
+
+
+class FeatureWiseAffine(nn.Module):
+    def __init__(self, in_channels, out_channels, use_affine_level=False):
+        super(FeatureWiseAffine, self).__init__()
+        self.use_affine_level = use_affine_level
+        self.noise_func = nn.Sequential(
+            nn.Linear(in_channels, out_channels*(1+self.use_affine_level))
+        )
+
+    def forward(self, x, noise_embed):
+        batch = x.shape[0]
+        if self.use_affine_level:
+            gamma, beta = self.noise_func(noise_embed).view(batch, -1, 1, 1).chunk(2, dim=1)
+            x = (1 + gamma) * x + beta
+        else:
+            x = x + self.noise_func(noise_embed).view(batch, -1, 1, 1)
+        return x
+
+
+class Swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+
+class Upsample(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.up = nn.Upsample(scale_factor=2, mode="nearest")
+        self.conv = nn.Conv2d(dim, dim, 3, padding=1)
+
+    def forward(self, x):
+        return self.conv(self.up(x))
+
+
+class Downsample(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.conv = nn.Conv2d(dim, dim, 3, 2, 1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+
+# building block modules
+
+
+class Block(nn.Module):
+    def __init__(self, dim, dim_out, groups=32, dropout=0):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.GroupNorm(groups, dim),
+            Swish(),
+            nn.Dropout(dropout) if dropout != 0 else nn.Identity(),
+            nn.Conv2d(dim, dim_out, 3, padding=1)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+
+class ResnetBlock(nn.Module):
+    def __init__(self, dim, dim_out, noise_level_emb_dim=None, dropout=0, use_affine_level=False, norm_groups=32):
+        super().__init__()
+        self.noise_func = FeatureWiseAffine(noise_level_emb_dim, dim_out, use_affine_level)
+
+        self.block1 = Block(dim, dim_out, groups=norm_groups)
+        self.block2 = Block(dim_out, dim_out, groups=norm_groups, dropout=dropout)
+        self.res_conv = nn.Conv2d(
+            dim, dim_out, 1) if dim != dim_out else nn.Identity()
+
+    def forward(self, x, time_emb):
+        b, c, h, w = x.shape
+        h = self.block1(x)
+        h = self.noise_func(h, time_emb)
+        h = self.block2(h)
+        return h + self.res_conv(x)
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, in_channel, n_head=1, norm_groups=32):
+        super().__init__()
+
+        self.n_head = n_head
+
+        self.norm = nn.GroupNorm(norm_groups, in_channel)
+        self.qkv = nn.Conv2d(in_channel, in_channel * 3, 1, bias=False)
+        self.out = nn.Conv2d(in_channel, in_channel, 1)
+
+    def forward(self, input):
+        batch, channel, height, width = input.shape
+        n_head = self.n_head
+        head_dim = channel // n_head
+
+        norm = self.norm(input)
+        qkv = self.qkv(norm).view(batch, n_head, head_dim * 3, height, width)
+        query, key, value = qkv.chunk(3, dim=2)  # bhdyx
+
+        attn = torch.einsum("bnchw, bncyx -> bnhwyx", query, key).contiguous() / math.sqrt(channel)
+        attn = attn.view(batch, n_head, height, width, -1)
+        attn = torch.softmax(attn, -1)
+        attn = attn.view(batch, n_head, height, width, height, width)
+
+        out = torch.einsum("bnhwyx, bncyx -> bnchw", attn, value).contiguous()
+        out = self.out(out.view(batch, channel, height, width))
+
+        return out + input
+
+
+class ResnetBlocWithAttn(nn.Module):
+    def __init__(self, dim, dim_out, *, noise_level_emb_dim=None, norm_groups=32, dropout=0, with_attn=False):
+        super().__init__()
+        self.with_attn = with_attn
+        self.res_block = ResnetBlock(
+            dim, dim_out, noise_level_emb_dim, norm_groups=norm_groups, dropout=dropout)
+        if with_attn:
+            self.attn = SelfAttention(dim_out, norm_groups=norm_groups)
+
+    def forward(self, x, time_emb):
+        x = self.res_block(x, time_emb)
+        if(self.with_attn):
+            x = self.attn(x)
+        return x
+    
+def exists(x):
+    return x is not None
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+def extract(a, t, x_shape=(1,1,1,1)):
+    b, *_ = t.shape
+    out = a.gather(-1, t)
+    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
+
+# beta_schedule function
+def _warmup_beta(linear_start, linear_end, n_timestep, warmup_frac):
+    betas = linear_end * np.ones(n_timestep, dtype=np.float64)
+    warmup_time = int(n_timestep * warmup_frac)
+    betas[:warmup_time] = np.linspace(
+        linear_start, linear_end, warmup_time, dtype=np.float64)
+    return betas
+
+def make_beta_schedule(schedule, n_timestep, linear_start=1e-6, linear_end=1e-2, cosine_s=8e-3):
+    if schedule == 'quad':
+        betas = np.linspace(linear_start ** 0.5, linear_end ** 0.5,
+                            n_timestep, dtype=np.float64) ** 2
+    elif schedule == 'linear':
+        betas = np.linspace(linear_start, linear_end,
+                            n_timestep, dtype=np.float64)
+    elif schedule == 'warmup10':
+        betas = _warmup_beta(linear_start, linear_end,
+                             n_timestep, 0.1)
+    elif schedule == 'warmup50':
+        betas = _warmup_beta(linear_start, linear_end,
+                             n_timestep, 0.5)
+    elif schedule == 'const':
+        betas = linear_end * np.ones(n_timestep, dtype=np.float64)
+    elif schedule == 'jsd':  # 1/T, 1/(T-1), 1/(T-2), ..., 1
+        betas = 1. / np.linspace(n_timestep,
+                                 1, n_timestep, dtype=np.float64)
+    elif schedule == "cosine":
+        timesteps = (
+            torch.arange(n_timestep + 1, dtype=torch.float64) /
+            n_timestep + cosine_s
+        )
+        alphas = timesteps / (1 + cosine_s) * math.pi / 2
+        alphas = torch.cos(alphas).pow(2)
+        alphas = alphas / alphas[0]
+        betas = 1 - alphas[1:] / alphas[:-1]
+        betas = betas.clamp(max=0.999)
+    else:
+        raise NotImplementedError(schedule)
+    return betas
